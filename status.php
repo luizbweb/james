12ae318af9b6a92c090eb2cb9a9941c58186e1d3
luizbweb <luizbweb@gmail.com>
2017-10-24 14:13:37 -0200

    Adiciona cores conforme o status

--- status.php
+++ status.php
@@ -21,10 +21,13 @@ $out = curl_exec($curl);
 // Obtém a resposta HTTP
 $response = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-// Exibe a resposta
-echo $response;
+// Exibe a resposta. Implementar teste de diversos códigos HTTP
+echo "<h2 style='color:darkcyan;'>".$response." : </h2>";
 
-
-
+if ($response == '404') {
+	echo "<h2 style='color:darkred;'>O site está fora do ar, Sir...</h2>";
+} else {
+	echo "<h2 style='color:darkgreen;'>Está tudo bem, Sir...</h2>";
+}
 
 ?>
